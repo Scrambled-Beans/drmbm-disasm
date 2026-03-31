05f69e101578d965ad508e37ba863c7c797a5329
Scrambled Beans <scrambled-beans@outlook.com>
2026-03-31 20:53:21 +0100

    Add files via upload

---
+++ resources/palettes/palette table.asm
@@ -0,0 +1,273 @@
+; --------------------------------------------------------------
+; Palette table
+; --------------------------------------------------------------
+
+Pal_Black:
+	incbin	"resources/palettes/fix/0 - Black.pal"
+	
+Pal_RedYellowPuyos:
+	incbin	"resources/palettes/fix/1 - Red Yellow Puyos.pal"
+	
+Pal_BluePurplePuyos:
+	incbin	"resources/palettes/fix/2 - Blue Purple Puyos.pal"
+	
+Pal_GreenTealPuyos:
+	incbin	"resources/palettes/fix/3 - Green Teal Puyos.pal"
+	
+Pal_Characters_Puyo:
+	incbin	"resources/palettes/fix/4 - Characters - Puyo.pal"
+	
+Pal_IntroSky1_Puyo:
+	incbin	"resources/palettes/fix/5 - Intro Sky 1 - Puyo.pal"
+	
+Pal_IntroScenery1_Puyo:
+	incbin	"resources/palettes/fix/6 - Intro Scenery 1 - Puyo.pal"
+	
+Pal_IntroScenery2_Puyo:
+	incbin	"resources/palettes/fix/7 - Intro Scenery 2 - Puyo.pal"
+	
+Pal_IntroSky2_Puyo:
+	incbin	"resources/palettes/fix/8 - Intro Sky 2 - Puyo.pal"
+	
+Pal_IntroScenery3_Puyo:
+	incbin	"resources/palettes/fix/9 - Intro Scenery 3 - Puyo.pal"
+	
+Pal_GameOverFG:
+	incbin	"resources/palettes/fix/10 - Game Over FG.pal"
+	
+Pal_GameOverBG:
+	incbin	"resources/palettes/fix/11 - Game Over BG.pal"
+	
+Pal_White:
+	incbin	"resources/palettes/fix/12 - White 1.pal"
+	
+Pal_White2:
+	incbin	"resources/palettes/fix/13 - White 2.pal"
+	
+Pal_CabinBoard_Puyo:
+	incbin	"resources/palettes/fix/14 - Cabin Board - Puyo.pal"
+	
+Pal_EndingBG_Puyo:
+	incbin	"resources/palettes/fix/15 - Ending BG - Puyo.pal"
+	
+Pal_TitleArle_Puyo:
+	incbin	"resources/palettes/fix/16 - Title Arle - Puyo.pal"
+	
+Pal_StaffBG_Puyo:
+	incbin	"resources/palettes/fix/17 - Staff BG - Puyo.pal"
+	
+Pal_HighScoresBG:
+	incbin	"resources/palettes/fix/18 - High Scores BG.pal"
+	
+Pal_EndingArleNormal_Puyo:
+	incbin	"resources/palettes/fix/19 - Ending Arle Normal - Puyo.pal"
+	
+Pal_EndingArleFade_Puyo:
+	incbin	"resources/palettes/fix/20 - Ending Arle Fade - Puyo.pal"
+	
+Pal_Options:
+	incbin	"resources/palettes/fix/21 - Options.pal"
+	
+Pal_Unknown1_Puyo:
+	incbin	"resources/palettes/fix/22 - Unknown 1 - Puyo.pal"
+	
+Pal_MainMenu_Puyo:
+	incbin	"resources/palettes/fix/23 - Main Menu - Puyo.pal"
+	
+Pal_PracticeMode_Puyo:
+	incbin	"resources/palettes/fix/24 - Practice Mode - Puyo.pal"
+	
+Pal_OpponentPreview_Puyo:
+	incbin	"resources/palettes/fix/25 - Opponent Preview - Puyo.pal"
+	
+Pal_OptTextGray:
+	incbin	"resources/palettes/fix/26 - Options Gray Blue - Unused.pal"
+	
+Pal_OptTextBlue:
+	incbin	"resources/palettes/fix/27 - Options Text Blue.pal"
+	
+Pal_OptTextRed:
+	incbin	"resources/palettes/fix/28 - Options Text Red.pal"
+	
+Pal_OptTextGreen:
+	incbin	"resources/palettes/fix/29 - Options Text Green.pal"
+	
+Pal_TutorialJoystick:
+	incbin	"resources/palettes/fix/30 - Tutorial Joystick.pal"
+	
+Pal_TitleBG_Puyo:
+	incbin	"resources/palettes/fix/31 - Title BG - Puyo.pal"
+	
+Pal_Unknown2_Puyo:
+	incbin	"resources/palettes/fix/32 - Unknown 2 - Puyo.pal"
+	
+Pal_TitleBlue_Puyo:
+	incbin	"resources/palettes/fix/33 - Title Blue - Puyo.pal"
+	
+Pal_TitleGreen_Puyo:
+	incbin	"resources/palettes/fix/34 - Title Green - Puyo.pal"
+	
+Pal_TitleRed_Puyo:
+	incbin	"resources/palettes/fix/35 - Title Red - Puyo.pal"
+	
+Pal_TitleOrange_Puyo:
+	incbin	"resources/palettes/fix/36 - Title Orange - Puyo.pal"
+	
+Pal_TitleCyan_Puyo:
+	incbin	"resources/palettes/fix/37 - Title Cyan - Puyo.pal"
+	
+Pal_TitlePink_Puyo:
+	incbin	"resources/palettes/fix/38 - Title Pink - Puyo.pal"
+	
+Pal_StoneBoard1_Puyo:
+	incbin	"resources/palettes/fix/39 - Stone Board 1 - Puyo.pal"
+	
+Pal_StoneBoard2_Puyo:
+	incbin	"resources/palettes/fix/40 - Stone Board 2 - Puyo.pal"
+	
+Pal_RobotnikIntro:
+	incbin	"resources/palettes/fix/41 - Robotnik Intro.pal"
+	
+Pal_Scratch:
+	incbin	"resources/palettes/fix/42 - Scratch.pal"
+	
+Pal_Frankly:
+	incbin	"resources/palettes/fix/43 - Frankly.pal"
+	
+Pal_Coconuts:
+	incbin	"resources/palettes/fix/44 - Coconuts.pal"
+	
+Pal_Dynamight:
+	incbin	"resources/palettes/fix/45 - Dynamight.pal"
+	
+Pal_Grounder:
+	incbin	"resources/palettes/fix/46 - Grounder.pal"
+	
+Pal_DavySprocket:
+	incbin	"resources/palettes/fix/47 - Davy Sprocket.pal"
+	
+Pal_Spike:
+	incbin	"resources/palettes/fix/48 - Spike.pal"
+	
+Pal_CoconutsIntro:
+	incbin	"resources/palettes/fix/49 - Coconuts Intro.pal"
+	
+Pal_LevelIntroFG:
+	incbin	"resources/palettes/fix/50 - Level Intro FG.pal"
+	
+Pal_LevelIntroBG:
+	incbin	"resources/palettes/fix/51 - Level Intro BG.pal"
+	
+Pal_RobotnikLair:
+	incbin	"resources/palettes/fix/52 - Robotnik Lair.pal"
+	
+Pal_GameIntroGrounder:
+	incbin	"resources/palettes/fix/53 - Game Intro Grounder.pal"
+	
+Pal_GameIntroRobotnik:
+	incbin	"resources/palettes/fix/54 - Game Intro Robotnik.pal"
+	
+Pal_GameIntroScratch:
+	incbin	"resources/palettes/fix/55 - Game Intro Scratch.pal"
+	
+Pal_MainMenu:
+	incbin	"resources/palettes/fix/56 - Main Menu.pal"
+	
+Pal_MainMenuSel:
+	incbin	"resources/palettes/fix/57 - Main Menu Select.pal"
+	
+Pal_GameOverText:
+	incbin	"resources/palettes/fix/58 - Game Over Text.pal"
+	
+Pal_MainMenuShadow:
+	incbin	"resources/palettes/fix/59 - Main Menu Shadow.pal"
+	
+Pal_DragonBreath:
+	incbin	"resources/palettes/fix/60 - Dragon Breath.pal"
+	
+Pal_FranklyIntro:
+	incbin	"resources/palettes/fix/61 - Frankly Intro.pal"
+	
+Pal_Humpty:
+	incbin	"resources/palettes/fix/62 - Humpty.pal"
+	
+Pal_Robotnik:
+	incbin	"resources/palettes/fix/63 - Robotnik.pal"
+	
+Pal_Skweel:
+	incbin	"resources/palettes/fix/64 - Skweel.pal"
+	
+Pal_DavySprocketIntro:
+	incbin	"resources/palettes/fix/65 - Davy Sprocket Intro.pal"
+	
+Pal_DynamightIntro:
+	incbin	"resources/palettes/fix/66 - Dynamight Intro.pal"
+	
+Pal_SirFfuzzyLogik:
+	incbin	"resources/palettes/fix/67 - Sir Ffuzzy-Logik.pal"
+	
+Pal_Arms:
+	incbin	"resources/palettes/fix/68 - Arms.pal"
+	
+Pal_ArmsIntro:
+	incbin	"resources/palettes/fix/69 - Arms Intro.pal"
+	
+Pal_DifficultyFaces:
+	incbin	"resources/palettes/fix/70 - Difficulty Faces.pal"
+	
+Pal_SpikeIntro:
+	incbin	"resources/palettes/fix/71 - Spike Intro.pal"
+	
+Pal_DragonBreathIntro:
+	incbin	"resources/palettes/fix/72 - Dragon Breath Intro.pal"
+	
+Pal_SirFfuzzyLogikIntro:
+	incbin	"resources/palettes/fix/73 - Sir Ffuzzy-Logik Intro.pal"
+	
+Pal_HumptyIntro:
+	incbin	"resources/palettes/fix/74 - Humpty Intro.pal"
+	
+Pal_GrounderIntro:
+	incbin	"resources/palettes/fix/75 - Grounder Intro.pal"
+	
+Pal_VsOhNo:
+	incbin	"resources/palettes/fix/76 - VS Oh No.pal"
+	
+Pal_SkweelIntro:
+	incbin	"resources/palettes/fix/77 - Skweel Intro.pal"
+	
+Pal_ScratchIntro:
+	incbin	"resources/palettes/fix/78 - Scratch Intro.pal"
+	
+Pal_Title1:
+	incbin	"resources/palettes/fix/79 - Title 1.pal"
+	
+Pal_Title2:
+	incbin	"resources/palettes/fix/80 - Title 2.pal"
+	
+Pal_Title3:
+	incbin	"resources/palettes/fix/81 - Title 3.pal"
+	
+Pal_TitleB:
+	incbin	"resources/palettes/fix/82 - Title B.pal"
+	
+Pal_Password4:
+	incbin	"resources/palettes/fix/83 - Password 4.pal"
+	
+Pal_Password3:
+	incbin	"resources/palettes/fix/84 - Password 3.pal"
+	
+Pal_EndingBeans:
+	incbin	"resources/palettes/fix/85 - Ending Beans.pal"
+	
+Pal_SegaLogo:
+	incbin	"resources/palettes/fix/86 - Sega Logo.pal"
+	
+Pal_Credits:
+	incbin	"resources/palettes/fix/87 - Credits.pal"
+	
+Pal_TitleA:
+	incbin	"resources/palettes/fix/88 - Title A.pal"
+	
+Pal_HasBeanShadow:
+	incbin	"resources/palettes/fix/89 - Has Bean Shadow.pal"
