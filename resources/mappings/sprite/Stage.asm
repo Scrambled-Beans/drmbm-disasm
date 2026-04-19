b083d2ac728802a2f24cafbf53c524b98871dd6a
Scrambled Beans <scrambled-beans@outlook.com>
2026-04-19 17:30:47 +0100

    Add files via upload

---
+++ resources/mappings/sprite/Stage.asm
@@ -0,0 +1,389 @@
+Sprite_Stage:    mappingsTable
+
+	mappingsTableEntry.l	Sprite_Stage_0
+	mappingsTableEntry.l	Sprite_Stage_1
+	mappingsTableEntry.l	Sprite_Stage_2
+	mappingsTableEntry.l	Sprite_Stage_3
+	mappingsTableEntry.l	Sprite_Stage_4
+	mappingsTableEntry.l	0
+	mappingsTableEntry.l	Sprite_Stage_6
+	mappingsTableEntry.l	Sprite_Stage_7
+	mappingsTableEntry.l	Sprite_Stage_8
+	mappingsTableEntry.l	Sprite_Stage_9
+	mappingsTableEntry.l	Sprite_Stage_10
+	mappingsTableEntry.l	0
+	mappingsTableEntry.l	Sprite_Stage_12
+	mappingsTableEntry.l	Sprite_Stage_13
+	mappingsTableEntry.l	Sprite_Stage_14
+	mappingsTableEntry.l	Sprite_Stage_15
+	mappingsTableEntry.l	Sprite_Stage_16
+	mappingsTableEntry.l	Sprite_Stage_17
+	mappingsTableEntry.l	Sprite_Stage_18
+	mappingsTableEntry.l	Sprite_Stage_19
+	mappingsTableEntry.l	Sprite_Stage_20
+	mappingsTableEntry.l	Sprite_Stage_21
+	mappingsTableEntry.l	Sprite_Stage_22
+	mappingsTableEntry.l	Sprite_Stage_23
+	mappingsTableEntry.l	Sprite_Stage_24
+	mappingsTableEntry.l	0
+	mappingsTableEntry.l	Sprite_Stage_26
+	mappingsTableEntry.l	Sprite_Stage_27
+	mappingsTableEntry.l	Sprite_Stage_28
+	mappingsTableEntry.l	Sprite_Stage_29
+	mappingsTableEntry.l	Sprite_Stage_30
+	mappingsTableEntry.l	0
+	mappingsTableEntry.l	Sprite_Stage_32
+	mappingsTableEntry.l	Sprite_Stage_33
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_32:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -24, -4, 1, 2, $534, 0, 0, 0, 1, 0
+	spritePiece -16, -4, 1, 2, $516, 0, 0, 0, 1, 0
+	spritePiece -8, -4, 1, 2, $53E, 0, 0, 0, 1, 0
+	spritePiece 0, -4, 1, 2, $53A, 0, 0, 0, 1, 0
+	spritePiece 8, -4, 1, 2, $51E, 0, 0, 0, 1, 0
+	spritePiece 16, -4, 1, 2, $51C, 0, 0, 0, 1, 0
+
+Sprite_Stage_32_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_33:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -24, -4, 1, 2, $534, 0, 0, 1, 1, 0
+	spritePiece -16, -4, 1, 2, $516, 0, 0, 1, 1, 0
+	spritePiece -8, -4, 1, 2, $53E, 0, 0, 1, 1, 0
+	spritePiece 0, -4, 1, 2, $53A, 0, 0, 1, 1, 0
+	spritePiece 8, -4, 1, 2, $51E, 0, 0, 1, 1, 0
+	spritePiece 16, -4, 1, 2, $51C, 0, 0, 1, 1, 0
+
+Sprite_Stage_33_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_4:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece 36, -4, 1, 1, $201, 0, 0, 0, 1, 0
+	spritePiece 0, 0, 4, 4, $220, 0, 0, 0, 1, 1
+	spritePiece 32, 0, 4, 4, $230, 0, 0, 0, 1, 1
+	spritePiece 64, 0, 4, 4, $240, 0, 0, 0, 1, 1
+	spritePiece 96, 0, 3, 4, $250, 0, 0, 0, 1, 1
+	spritePiece 0, 32, 4, 3, $260, 0, 0, 0, 1, 1
+	spritePiece 32, 32, 4, 3, $26C, 0, 0, 0, 1, 1
+	spritePiece 64, 32, 4, 3, $278, 0, 0, 0, 1, 1
+	spritePiece 96, 32, 3, 3, $284, 0, 0, 0, 1, 1
+
+Sprite_Stage_4_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_16:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece 0, 0, 4, 2, $488, 0, 0, 3, 1, 0
+
+Sprite_Stage_16_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_17:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -12, 1, 1, $314, 0, 0, 2, 0, 0
+	spritePiece 0, -12, 1, 1, $314, 1, 0, 2, 0, 0
+	spritePiece -16, -4, 1, 1, $315, 0, 0, 2, 0, 3
+	spritePiece 8, -4, 1, 1, $315, 1, 0, 2, 0, 3
+
+Sprite_Stage_17_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_18:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -12, 1, 1, $314, 0, 0, 2, 0, 0
+	spritePiece 0, -12, 1, 1, $314, 1, 0, 2, 0, 0
+	spritePiece -16, -2, 1, 1, $316, 0, 0, 2, 0, 3
+	spritePiece 8, -2, 1, 1, $316, 1, 0, 2, 0, 3
+
+Sprite_Stage_18_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_19:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -12, 1, 1, $314, 0, 0, 2, 0, 0
+	spritePiece 0, -12, 1, 1, $314, 1, 0, 2, 0, 0
+	spritePiece -16, 0, 1, 1, $317, 0, 0, 2, 0, 3
+	spritePiece 8, 0, 1, 1, $317, 1, 0, 2, 0, 3
+
+Sprite_Stage_19_End
+
+; ---------------------------------------------------------------------------
+Sprite_Stage_20:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_20_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_21:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -24, 2, 2, $2F8, 0, 0, 2, 0, 2
+
+Sprite_Stage_21_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_22:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -24, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -40, 2, 2, $2F8, 0, 0, 2, 0, 2
+
+Sprite_Stage_22_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_23:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -24, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -40, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -56, 2, 2, $2F8, 0, 0, 2, 0, 2
+
+Sprite_Stage_23_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_24:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -24, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -40, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -56, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -8, -72, 2, 2, $2F8, 0, 0, 2, 0, 2
+
+Sprite_Stage_24_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_26:	spriteHeaderEnter 2
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_26_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_27:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -18, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_27_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_28:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -18, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -34, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_28_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_29:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -18, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -34, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -50, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_29_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_30:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -18, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -34, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -50, 2, 2, $310, 0, 0, 3, 0, 3
+	spritePiece -2, -66, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_30_End
+
+; ---------------------------------------------------------------------------
+Sprite_Stage_0:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $2F8, 0, 0, 2, 0, 2
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_0_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_1:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $2FC, 0, 0, 2, 0, 2
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_1_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_2:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $300, 0, 0, 2, 0, 2
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_2_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_3:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $304, 0, 0, 2, 0, 2
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_3_End
+
+; ---------------------------------------------------------------------------
+Sprite_Stage_6:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_6_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_7:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_7_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_8:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_8_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_9:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -2, -2, 2, 2, $310, 0, 0, 3, 0, 3
+
+Sprite_Stage_9_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_12:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $F8, 0, 0, 3, 1, 0
+
+Sprite_Stage_12_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_13:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -8, -8, 2, 2, $FC, 0, 0, 3, 1, 0
+
+Sprite_Stage_13_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_14:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -4, -4, 1, 1, $32A, 0, 0, 0, 1, 0
+
+Sprite_Stage_14_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_15:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -4, -4, 1, 1, $32A, 0, 0, 0, 1, 3
+
+Sprite_Stage_15_End
+
+; ---------------------------------------------------------------------------
+
+Sprite_Stage_10:	spriteHeader
+
+	; X, Y, Width, Height, Tile, X Flip, Y Flip, Palette, Priority, Link
+
+	spritePiece -4, -4, 1, 1, $32A, 0, 0, 0, 0, 3
+
+Sprite_Stage_10_End
+
+; ---------------------------------------------------------------------------
+
+	even
